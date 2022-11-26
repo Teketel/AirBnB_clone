86e482415b294f511d26294b76e17b19aa792401
= <=>
2022-11-26 15:54:57 +0300

    Done tasks, for testing

---
+++ README.md
@@ -0,0 +1,5 @@
+AirBnB_clone
+====
+
+This project is to build a clone of the AirBnB website. It implements a `console`, Command-Line-Interface(CLI) to manage program data. CLI allow to input commands to create, update, and destroy objects in `file storage`.
+
---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains classes, functions and constants related to amenity."""
+from models.base_model import BaseModel
+
+
+
+class Amenity(BaseModel):
+    """This Amenity class contains amenity related attributes to store."""
+    name = ""
